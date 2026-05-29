e43fe8a229fa0f10b40eb14e6b18abf4011d2487
cute Z <aryansatyam.mainacc@gmail.com>
2026-05-29 16:05:14 +0530

    End of 29th May

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -4,6 +4,6 @@ extends RigidBody2D
 var ball_speed = 400
 var ball_direction = Vector2(-1, 0)
 
-func _process(delta: float) -> void:
+func _ready():
 	var velocity = ball_speed * ball_direction
 	linear_velocity = velocity
